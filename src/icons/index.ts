25c83fc516f006e422608c479d7d4557b50cf3c1
hhyunn <cha9164@naver.com>
2024-06-24 14:49:32 +0900

    Feat: TextButton 컴포넌트 생성

---
+++ src/icons/index.ts
@@ -0,0 +1,29 @@
+import IconAi from "@/icons/IconAi";
+import IconArrow from "@/icons/IconArrow";
+import IconCalender from "@/icons/IconCalender";
+import IconClose from "@/icons/IconClose";
+import IconDown from "@/icons/IconDown";
+import IconEdit from "@/icons/IconEdit";
+import IconEyeHide from "@/icons/IconEyeHide";
+import IconEyeShow from "@/icons/IconEyeShow";
+import IconPlus from "@/icons/IconPlus";
+import IconRefresh from "@/icons/IconRefresh";
+import IconSearch from "@/icons/IconSearch";
+import IconTime from "@/icons/IconTime";
+import IconTranslate from "@/icons/IconTranslate";
+
+export {
+    IconAi,
+    IconArrow,
+    IconCalender,
+    IconClose,
+    IconDown,
+    IconEdit,
+    IconEyeHide,
+    IconEyeShow,
+    IconPlus,
+    IconRefresh,
+    IconSearch,
+    IconTime,
+    IconTranslate,
+};
