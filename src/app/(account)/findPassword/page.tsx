88dc4a420e6c7bce4bb0ea99a771b649f93f6e63
chaeney <chaekey@gmail.com>
2024-06-25 23:14:41 +0900

    Feat: find password / 임시 비밀번호 발급 모달페이지 생성

--- src/app/(account)/findPassword/page.tsx
+++ src/app/(account)/findPassword/page.tsx
@@ -1,6 +1,3 @@
-import TextButton from "@/components/Button/TextButton";
-import Popup from "@/components/Popup";
-import PopupTitle from "@/components/PopupTitle";
 import AccountFormBox from "@/containers/account/AccountFormBox";
 import FindPasswordForm from "@/containers/account/findPassword/FindPasswordForm";
 
@@ -13,14 +10,6 @@ export default function FindPassword() {
         </h3>
 
         <FindPasswordForm />
-
-        <Popup title="임시 비밀번호가 발급되었습니다.">
-          <PopupTitle
-            subTitle={`이메일을 확인하여 임시 비밀번호로
-재로그인 후 비밀번호를 변경해주세요.`}
-          ></PopupTitle>
-          <TextButton>오케이</TextButton>
-        </Popup>
       </AccountFormBox>
     </>
   );
