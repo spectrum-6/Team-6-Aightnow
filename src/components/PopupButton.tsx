123372c959af22579cb994414d541432e6d48170
Shin-E2 <sinhyejee322@gmail.com>
2024-06-25 17:53:18 +0900

    Style: Change CSS bg color and set width

--- src/components/PopupButton.tsx
+++ src/components/PopupButton.tsx
@@ -4,6 +4,7 @@ import React, { FC } from 'react';
 type TPopupButtonProps = {
  buttonLeft: string;
  buttonRight: string;
+ normalButton?: string;
 };
 
 // 함수 선언 컴포넌트 사용
@@ -11,11 +12,11 @@ function PopupButton({ buttonLeft, buttonRight }: TPopupButtonProps) {
  return (
   <div className="flex justify-center items-center gap-2">
    {/* 왼쪽 버튼 */}
-   <button className="h-14 px-4 py-2 bg-[#E9E9E9] rounded-lg flex justify-center items-center text-[#575757] text-base font-medium font-pretendard leading-normal">
+   <button className="w-40 h-14 px-4 py-2 bg-[#E9E9E9] rounded-lg flex justify-center items-center text-[#575757] text-base font-medium font-pretendard leading-normal">
     {buttonLeft}
    </button>
    {/* 오른쪽 버튼 */}
-   <button className="h-14 px-4 py-2 bg-black rounded-lg flex justify-center items-center text-[#FFFFFF] text-base font-medium font-Pretendard leading-normal">
+   <button className="w-40 h-14 px-4 py-2 bg-[#18254C] rounded-lg flex justify-center items-center text-[#FFFFFF] text-base font-medium font-Pretendard leading-normal">
     {buttonRight}
    </button>
   </div>
