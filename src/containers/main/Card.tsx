14f9fd84173c3177a43a3181e4a322f9eb1e154a
Shin-E2 <sinhyejee322@gmail.com>
2024-06-29 01:58:27 +0900

    Feat: Create common components folder for main page

---
+++ src/containers/main/Card.tsx
@@ -0,0 +1,55 @@
+import { IconApple } from "@/icons";
+import React from "react";
+
+const Card: React.FC = () => {
+  return (
+    <div className="flex flex-col p-8 bg-white rounded-lg w-[387px] h-[304px] gap-[16px]">
+      {/* 1. 애플 */}
+      <div className="flex flex-col ">
+        <div className="flex gap-[8px]">
+          <IconApple width={32} height={32} />
+          <div className="flex gap-[8px] items-center ">
+            <span className="text-2xl font-bold">애플</span>
+            <span className="text-gray-500 text-lg">AAPL</span>
+          </div>
+        </div>
+        <div className="flex gap-2">
+          <span className="text-base font-medium">$00.00</span>
+          <span className="text-red-500 text-base font-regular">▲1.75</span>
+          <span className="text-red-500 text-base font-regular">+0.82%</span>
+        </div>
+      </div>
+      {/* 2. 차트box */}
+      <div className="flex gap-[24px]">
+        <div className="w-[168px] h-[168px] bg-blue-100"></div>
+        {/* 차트 list */}
+        <div className="flex flex-col p-[16px] bg-gray-100 rounded-lg w-[168px] h-[168px]">
+          <ul className="flex flex-col space-y-1">
+            <li className="flex justify-between items-center">
+              <span className="text-base font-medium">주가</span>
+              <span className="text-blue-500 text-sm font-medium">▼0.0%</span>
+            </li>
+            <li className="flex justify-between items-center">
+              <span className="text-base font-medium">투자지수</span>
+              <span className="text-red-500  text-sm font-medium">▲0.0%</span>
+            </li>
+            <li className="flex justify-between gap-1  items-center">
+              <span className="text-base font-medium">수익성</span>
+              <span className="text-red-500  text-sm font-medium">▲0.0%</span>
+            </li>
+            <li className="flex justify-between  items-center">
+              <span className="text-base font-medium">성장성</span>
+              <span className="text-red-500  text-sm font-medium">▲0.0%</span>
+            </li>
+            <li className="flex justify-between  items-center">
+              <span className="text-base font-medium">관심도</span>
+              <span className="text-red-500  text-sm font-medium">▲0.0%</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
