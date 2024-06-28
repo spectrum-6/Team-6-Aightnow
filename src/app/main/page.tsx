cfaa1f7464181ef5dd8005f9c92f1fe6668b0d77
Shin-E2 <sinhyejee322@gmail.com>
2024-06-29 01:56:58 +0900

    Feat: Create new main page

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,17 +1,19 @@
 import Header from "@/components/Header";
-import UserAIReport from '@/containers/account/main/UserAIReport';
+import RecentViews from "@/containers/main/RecentViews";
+import StockNews from "@/containers/main/StockNews";
+import UserAIReport from "@/containers/main/UserAIReport";
+import ChatBotBtn from "@/containers/main/chatbot/ChatBotBtn";
 
 export default function main() {
   return (
-      <div className="bg-grayscale-100 flex w-full flex-col gap-[5.88rem] md:gap-[4.38rem] sm:gap-[2.94m]">
-        <Header />
-        <main className="w-[1200px] mx-auto my-[56px] flex flex-col gap-12">
-      <UserAIReport />
-      <div className="flex gap-5">
-        <RecentViewed />
-        <FavoriteStocks />
-      </div>
-      <StockNews />
-    </main>
+    <div className="flex w-screen h-screen flex-col">
+      <Header />
+      <main className="w-[1200px] h-full mx-auto my-[174px] flex flex-col gap-12">
+        <UserAIReport />
+        <RecentViews />
+        <StockNews />
+      </main>
+      <ChatBotBtn />
+    </div>
   );
 }
