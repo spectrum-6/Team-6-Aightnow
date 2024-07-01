ea733e6a96deb95e7a8defdb25b2f35c2b2104a9
chaeney <chaekey@gmail.com>
2024-07-01 21:11:16 +0900

    Feat: modal URL 직접 접근 시 notFound 처리

--- src/app/[locale]/(account)/findPassword/success/page.tsx
+++ src/app/[locale]/(account)/findPassword/success/page.tsx
@@ -1,19 +1,5 @@
-import TextButton from "@/components/Button/TextButton";
-import Popup from "@/components/Popup/Popup";
-import PopupTitle from "@/components/Popup/PopupTitle";
-import Link from "next/link";
+import { notFound } from "next/navigation";
 
 export default function FindPasswordSuccess() {
-  return (
-    <>
-      <Popup title="임시 비밀번호가 발급되었습니다.">
-        <PopupTitle
-          subTitle={`이메일을 확인하여 임시 비밀번호로\n재로그인 후 비밀번호를 변경해주세요.`}
-        ></PopupTitle>
-        <Link href={"/login"}>
-          <TextButton>로그인</TextButton>
-        </Link>
-      </Popup>
-    </>
-  );
+  notFound();
 }
