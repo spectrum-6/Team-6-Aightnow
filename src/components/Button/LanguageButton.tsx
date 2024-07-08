081a7ed8a319dfa85c263289544998419d9cc9d2
chaeney <chaekey@gmail.com>
2024-07-08 18:52:21 +0900

    Feat: 마이페이지 언어 설정 기능 구현

--- src/components/Button/LanguageButton.tsx
+++ src/components/Button/LanguageButton.tsx
@@ -2,52 +2,61 @@ import { ButtonHTMLAttributes, useEffect, useState } from "react";
 import { IconKr, IconCn, IconFr, IconJp, IconUs } from "@/icons";
 
 type TLanguageButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-    lang: "kr" | "cn" | "fr" | "jp" | "us";
+  lang: "ko" | "en" | "zh" | "ja" | "fr";
+  selected: boolean;
 };
 
-export default function LanguageButton({ lang, ...rest }: TLanguageButtonProps) {
-    const getIcon = () => {
-        switch (lang) {
-            case "kr":
-                return <IconKr />;
-            case "cn":
-                return <IconCn />;
-            case "fr":
-                return <IconFr />;
-            case "jp":
-                return <IconJp />;
-            case "us":
-                return <IconUs />;
-            default:
-                return null;
-        }
-    };
+export default function LanguageButton({
+  lang,
+  selected,
+  ...rest
+}: TLanguageButtonProps) {
+  const getIcon = () => {
+    switch (lang) {
+      case "ko":
+        return <IconKr />;
+      case "zh":
+        return <IconCn />;
+      case "fr":
+        return <IconFr />;
+      case "ja":
+        return <IconJp />;
+      case "en":
+        return <IconUs />;
+      default:
+        return null;
+    }
+  };
 
-    const getText = () => {
-        switch (lang) {
-            case "kr":
-                return "한국어";
-            case "cn":
-                return "중국어";
-            case "fr":
-                return "프랑스어";
-            case "jp":
-                return "일본어";
-            case "us":
-                return "영어";
-            default:
-                return "";
-        }
-    };
+  const getText = () => {
+    switch (lang) {
+      case "ko":
+        return "한국어";
+      case "zh":
+        return "중국어";
+      case "fr":
+        return "프랑스어";
+      case "ja":
+        return "일본어";
+      case "en":
+        return "영어";
+      default:
+        return "";
+    }
+  };
 
-    return (
-        <button
-            className={
-                "w-[198px] h-40 border border-grayscale-300 rounded-2xl text-lg font-bold text-grayscale-300 flex flex-col items-center justify-center hover:border-blue-600 hover:text-blue-600"
-            }
-            {...rest}>
-            {getIcon()}
-            <span>{getText()}</span>
-        </button>
-    );
+  return (
+    <button
+      className={`w-[198px] h-40 border rounded-2xl text-lg font-bold  flex flex-col items-center justify-center  ${
+        selected
+          ? "border-blue-600 text-blue-600"
+          : "border-grayscale-300 text-grayscale-300 hover:border-blue-400 hover:text-blue-400"
+      }`}
+      disabled={selected}
+      {...rest}
+    >
+      {getIcon()}
+      <span>{getText()}</span>
+    </button>
+  );
 }
