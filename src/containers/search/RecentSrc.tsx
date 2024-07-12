43295449321468279cfdb709f79153dd8ef6989e
Hoi <3262398@gmail.com>
2024-07-13 02:41:37 +0900

    Feat: recentSearch 전체 삭제 구현

--- src/containers/search/RecentSrc.tsx
+++ src/containers/search/RecentSrc.tsx
@@ -58,7 +58,10 @@ export default function RecentSrc() {
     <div className="flex flex-col gap-2">
       <div className="flex justify-between items-center">
         <span className="text-navy-900 font-bold text-2xl">최근 검색어</span>
-        <span className="text-grayscale-600 font-medium border-b border-gray-600">
+        <span
+          className="text-grayscale-600 font-medium border-b border-gray-600 cursor-pointer"
+          onClick={handleDeleteAll}
+        >
           전체삭제
         </span>
       </div>
