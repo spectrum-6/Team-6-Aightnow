215fca2578ef23a0764d288d51f67b29b3179d68
Hoi <3262398@gmail.com>
2024-07-14 05:19:54 +0900

    Fix: 인기 검색어 구현 수정

--- src/containers/search/PopSrcList.tsx
+++ src/containers/search/PopSrcList.tsx
@@ -22,26 +22,6 @@ export default function PopSrcList({
           </span>
         </li>
       ))}
-      {/* <li className="flex gap-5 h-10 items-center">
-       <span className="text-base font-medium">1</span>
-         <span className="text-base font-medium text-gray-600">테슬라</span>
-       </li>
-       <li className="flex gap-5 h-10 items-center">
-        <span className="text-base font-medium">1</span>
-         <span className="text-base font-medium text-gray-600">테슬라</span>
-       </li>
-       <li className="flex gap-5 h-10 items-center">
-         <span className="text-base font-medium">1</span>
-       <span className="text-base font-medium text-gray-600">테슬라</span>
-       </li>
-      <li className="flex gap-5 h-10 items-center">
-         <span className="text-base font-medium">1</span>
-         <span className="text-base font-medium text-gray-600">테슬라</span>
-     </li>
-       <li className="flex gap-5 h-10 items-center">
-         <span className="text-base font-medium">1</span>
-         <span className="text-base font-medium text-gray-600">테슬라</span>
-       </li> */}
     </ul>
   );
 }
