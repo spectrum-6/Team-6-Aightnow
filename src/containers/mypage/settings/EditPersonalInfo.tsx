08f580b9b7b0d76c58a842641afa14a2bf99f79a
chaeney <chaekey@gmail.com>
2024-07-14 15:29:36 +0900

    Feat: 프로필수정 모달 관심종목 리스트만 표출

--- src/containers/mypage/settings/EditPersonalInfo.tsx
+++ src/containers/mypage/settings/EditPersonalInfo.tsx
@@ -19,6 +19,7 @@ export default function EditPersonalInfo({ userInfo }: { userInfo: UserInfo }) {
             </div>
             <div className="flex lg:items-center lg:ml-4">
               <Link
+                scroll={false}
                 href="/settings/account/edit/editProfile"
                 className="flex items-center justify-center w-[160px] h-[36px] bg-black text-white text-sm py-2 px-[10px] rounded-lg"
               >
