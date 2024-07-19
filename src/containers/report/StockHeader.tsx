0d1ae3b9ee0435f3cc85b40f592d36635fa41216
hhyunn <cha9164@naver.com>
2024-07-19 23:18:37 +0900

    Feat: 리포트 페이지 관심종목 추가/해제 버튼 구현

--- src/containers/report/StockHeader.tsx
+++ src/containers/report/StockHeader.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   IconApple,
   IconTsla,
@@ -10,6 +10,9 @@ import {
   IconUnity,
   IconNvidia,
 } from "@/icons";
+import useUserStore from "@/stores/useUserStore";
+import { UserInfo } from "@/types/UserInfo";
+import { updateUserInfo } from "@/firebase/firestore";
 
 type TStockHeaderProps = {
   reutersCode: string;
@@ -20,10 +23,54 @@ type TStockHeaderProps = {
 export default function StockHeader(props: TStockHeaderProps) {
   const { reutersCode, stockName, symbolCode } = props;
 
-  const [isFavorite, setIsFavorite] = useState<boolean>(false);
+  // 관심종목 여부
+  const [isFavoriteStock, setIsFavoriteStock] = useState<boolean>(false);
 
-  const handleButtonClick = (): void => {
-    setIsFavorite(!isFavorite);
+  const { userInfo, setUserInfo } = useUserStore();
+
+  useEffect(() => {
+    if (userInfo?.userStockCollection?.watchList) {
+      setIsFavoriteStock(
+        userInfo.userStockCollection.watchList.includes(symbolCode),
+      );
+    }
+  }, [userInfo, symbolCode]);
+
+  const toggleFavoriteStock = async () => {
+    if (userInfo && userInfo.uid && userInfo.userStockCollection) {
+      let newWatchList: string[];
+
+      // 관심종목인 경우 삭제
+      if (isFavoriteStock) {
+        newWatchList = userInfo.userStockCollection.watchList.filter(
+          (item) => item !== symbolCode,
+        );
+        if (newWatchList.length === 0) {
+          alert("관심종목은 최소 1개 이상 설정되어야 합니다.");
+          return;
+        }
+      }
+      // 관심종목이 아닌 경우 추가
+      else {
+        newWatchList = [symbolCode, ...userInfo.userStockCollection.watchList];
+      }
+
+      // watchlist 업데이트
+      const updatedUserInfo: Partial<UserInfo> = {
+        ...userInfo,
+        userStockCollection: {
+          ...userInfo.userStockCollection,
+          watchList: newWatchList,
+        },
+      };
+
+      // DB 업데이트
+      await updateUserInfo(userInfo.uid, updatedUserInfo);
+      // 세션 정보 업데이트
+      setUserInfo(updatedUserInfo);
+      // 상태 업데이트
+      setIsFavoriteStock((prev) => !prev);
+    }
   };
 
   const getStockLogo = (reutersCode: string) => {
@@ -58,13 +105,13 @@ export default function StockHeader(props: TStockHeaderProps) {
       </div>
       <button
         className={`w-[180px] h-[56px] rounded-lg text-base font-medium ${
-          isFavorite
+          isFavoriteStock
             ? "border border-navy-900 text-navy-900"
             : "bg-navy-900 text-white"
         }`}
-        onClick={handleButtonClick}
+        onClick={toggleFavoriteStock}
       >
-        {isFavorite ? "관심종목 해제" : "관심종목 추가"}
+        {isFavoriteStock ? "관심종목 해제" : "관심종목 추가"}
       </button>
     </div>
   );
