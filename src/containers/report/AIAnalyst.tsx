227e9dcc6b072e4ba123edd24e33c97e3e599910
hhyunn <cha9164@naver.com>
2024-07-22 01:32:43 +0900

    Feat: AI 애널리스트 리포트에 데이터 렌더링

--- src/containers/report/AIAnalyst.tsx
+++ src/containers/report/AIAnalyst.tsx
@@ -15,6 +15,7 @@ type TAiAnalystProps = {
   closePrice: string;
   compareToPreviousClosePrice: string;
   fluctuationsRatio: string;
+  promptResult: any;
 };
 
 export default function AiAnalyst(props: TAiAnalystProps) {
@@ -25,7 +26,11 @@ export default function AiAnalyst(props: TAiAnalystProps) {
     closePrice,
     compareToPreviousClosePrice,
     fluctuationsRatio,
+    promptResult,
   } = props;
+
+  const analysis = promptResult.analysis;
+  const investmentOutlook = promptResult.investmentOutlook;
 
   const getStockLogo = (reutersCode: string) => {
     switch (reutersCode) {
@@ -103,18 +108,7 @@ export default function AiAnalyst(props: TAiAnalystProps) {
       {/* 설명 */}
       <div className="h-24 overflow-y-auto">
         <p className="font-medium">
-          "급격한 금리 인상에도 견조한 자동차 수요를 반영하여 테슬라의
-          목표주가를 340달러로 26% 상향 조정하고 Top Pick으로 유지한다. 단기
-          상승에 따른 숨 고르기가 예상되지만, 중기적으로 동사의 경쟁우위는 더
-          강해지고 있다. 기존 OEM의 전기차 전환이 더디고 중국 신생 업체들의 현금
-          흐름이 약화되고있는 가운데, 테슬라의 멕시코 공장이 가동되면 전기차
-          제조 경쟁력 격차는 더 벌어질 것으로 예상된다. 급격한 금리 인상에도
-          견조한 자동차 수요를 반영하여 테슬라의 목표주가를 340달러로 26% 상향
-          조정하고 Top Pick으로 유지한다. 단기 상승에 따른 숨 고르기가
-          예상되지만, 중기적으로 동사의 경쟁우위는 더 강해지고 있다. 기존 OEM의
-          전기차 전환이 더디고 중국 신생 업체들의 현금 흐름이 약화되고있는
-          가운데, 테슬라의 멕시코 공장이 가동되면 전기차 제조 경쟁력 격차는 더
-          벌어질 것으로 예상된다."
+          {analysis} {investmentOutlook}
         </p>
       </div>
     </div>
--- src/containers/report/ReportContainer.tsx
+++ src/containers/report/ReportContainer.tsx
@@ -72,6 +72,7 @@ export default function ReportContainer(props: TReportContainer) {
                   closePrice={closePrice}
                   compareToPreviousClosePrice={compareToPreviousClosePrice}
                   fluctuationsRatio={fluctuationsRatio}
+                  promptResult={promptResult}
                 />
               </div>
             </div>
