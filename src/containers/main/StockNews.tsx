8ec2d53fab8812ca5fa515a5f81df5d97d305a1b
hhyunn <cha9164@naver.com>
2024-07-23 16:46:01 +0900

    Feat: 메인 페이지 유저 닉네임으로 변경

--- src/containers/main/StockNews.tsx
+++ src/containers/main/StockNews.tsx
@@ -1,14 +1,19 @@
-// "use client";
+"use client";
 
+import useUserStore from "@/stores/useUserStore";
 import InterestNews from "./InterestNews";
 import MainNews from "./MainNews";
 import RecentNews from "./RecentNews";
 
 export default function StockNews() {
+  const { userInfo } = useUserStore();
+
   return (
     <>
       <div className="flex flex-col justify-start gap-6 w-[1200px]">
-        <h4 className="font-bold text-navy-900">스팩님을 위한 주식뉴스</h4>
+        <h4 className="font-bold text-navy-900">
+          {userInfo?.nickname}님을 위한 주식뉴스
+        </h4>
         <div className="flex gap-5 p-12 rounded-2xl bg-white">
           <div className="flex flex-col gap-12">
             {/* 관심 종목 뉴스 */}
--- src/containers/main/UserAIReport.tsx
+++ src/containers/main/UserAIReport.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import { Badge } from "@/components/Badge";
 import { IconAi } from "@/icons";
 import Card from "./Card";
+import useUserStore from "@/stores/useUserStore";
 
 export default function UserAIReport() {
+  const { userInfo } = useUserStore();
+
   return (
     <div className="flex flex-col gap-5">
       <div className="flex gap-4">
         <h4 className="font-bold text-navy-900 leading-9">
-          스팩님의 AI 리포트
+          {userInfo?.nickname}님의 AI 리포트
         </h4>
         <Badge variant="navy" icon={<IconAi />} />
       </div>
