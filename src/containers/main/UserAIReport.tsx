c092c8a55a207b83c0c81f58a70c063e2b1a36d5
hhyunn <cha9164@naver.com>
2024-07-23 18:00:05 +0900

    Feat: 메인 페이지 AI 리포트에 데이터 렌더링

--- src/containers/main/UserAIReport.tsx
+++ src/containers/main/UserAIReport.tsx
@@ -1,12 +1,86 @@
 "use client";
 
-import { Badge } from "@/components/Badge";
-import { IconAi } from "@/icons";
-import Card from "./Card";
+import { useEffect, useState } from "react";
 import useUserStore from "@/stores/useUserStore";
+import { getPromptData } from "./getPromptData";
+import { TStockType } from "@/types/stockType";
+import { Badge } from "@/components/Badge";
+import Card from "./Card";
+import { IconAi } from "@/icons";
+
+type TStocks = {
+  [key: string]: string; // index signature
+};
+
+const stocks: TStocks = {
+  AAPL: "aapl",
+  TSLA: "tsla",
+  AMZN: "amzn",
+  MSFT: "msft",
+  GOOGL: "googl",
+  U: "u",
+  NVDA: "nvda",
+};
+
+// DB에 저장된 stock 조회
+const getStockData = async (symbolCode: string) => {
+  const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+  try {
+    const response = await fetch(`${BASE_URL}/api/scheduleStock/${symbolCode}`);
+
+    return await response.json();
+  } catch (e) {
+    console.log("error : ", e);
+  }
+};
 
 export default function UserAIReport() {
+  // 세션에 저장된 유저 정보
   const { userInfo } = useUserStore();
+  const watchList = userInfo?.userStockCollection?.watchList; //symbolCode List
+
+  const [favoriteStock, setFavoriteStock] = useState<TStockType[]>([]);
+
+  useEffect(() => {
+    let list: TStockType[] = [];
+    if (watchList) {
+      watchList.map(async (symbolCode) => {
+        const result = await getStockData(symbolCode);
+
+        if (result) {
+          list.push(result);
+        }
+        setFavoriteStock([...list]);
+      });
+    }
+  }, [watchList]);
+
+  // 프롬프트 데이터 가져오기
+  const [promptResults, setPromptResults] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (
+        favoriteStock.length > 0 &&
+        favoriteStock.length === watchList?.length
+      ) {
+        const promises = favoriteStock.map(async (item) => {
+          const promptResult = await getPromptData(
+            stocks[item.symbolCode],
+            item.symbolCode,
+          );
+          console.log("!!!", promptResult);
+          return promptResult;
+        });
+
+        const resolvedResults: any = await Promise.all(promises);
+        setPromptResults(resolvedResults);
+      }
+    };
+
+    fetchData();
+  }, [favoriteStock]);
 
   return (
     <div className="flex flex-col gap-5">
@@ -17,9 +91,26 @@ export default function UserAIReport() {
         <Badge variant="navy" icon={<IconAi />} />
       </div>
       <div className="flex gap-5">
-        <Card />
-        <Card />
-        <Card />
+        {promptResults.length > 0
+          ? promptResults.map((item, index) => {
+              return (
+                <Card
+                  key={index}
+                  item={item}
+                  reutersCode={favoriteStock[index].reutersCode}
+                  stockName={favoriteStock[index].stockName}
+                  symbolCode={favoriteStock[index].symbolCode}
+                  closePrice={favoriteStock[index].closePrice}
+                  compareToPreviousClosePrice={
+                    favoriteStock[index].compareToPreviousClosePrice
+                  }
+                  fluctuationsRatio={favoriteStock[index].fluctuationsRatio}
+                />
+              );
+            })
+          : watchList?.map((_, index) => {
+              return <Card key={index} />;
+            })}
       </div>
     </div>
   );
