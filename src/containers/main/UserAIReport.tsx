82f8f9ca07b6349b914ff936c8a1297755a78d19
hhyunn <cha9164@naver.com>
2024-07-28 21:55:42 +0900

    Style: 레이아웃 수정

--- src/containers/main/UserAIReport.tsx
+++ src/containers/main/UserAIReport.tsx
@@ -83,7 +83,7 @@ export default function UserAIReport() {
   }, [favoriteStock]);
 
   return (
-    <div className="flex flex-col gap-5">
+    <div className="w-[1200px] flex flex-col gap-5">
       <div className="flex gap-4">
         <h4 className="font-bold text-navy-900 leading-9">
           {userInfo?.nickname}님의 AI 리포트
