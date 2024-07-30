b000950a6e36f384e64c74051a73f00ed2d38d83
chaeney <chaekey@gmail.com>
2024-07-30 18:04:35 +0900

    Refactor : 검색결과 Stock Icon 컴포넌트 적용

--- src/containers/search/searchAf/SrcStockList.tsx
+++ src/containers/search/searchAf/SrcStockList.tsx
@@ -1,10 +1,4 @@
-import IconApple from "@/icons/IconApple";
-import IconAmazon from "@/icons/IconAmazon";
-import IconGoogle from "@/icons/IconGoogle";
-import IconMs from "@/icons/IconMs";
-import IconNvidia from "@/icons/IconNvidia";
-import IconTesla from "@/icons/IconTsla"; // 여기서 IconTesla 임포트
-import IconUnity from "@/icons/IconUnity";
+import StockIcon from "@/components/StockIcon/StockIcon";
 
 type TSrcStockListProps = {
   stockName: string;
@@ -13,21 +7,6 @@ type TSrcStockListProps = {
   closePrice: string;
   compareToPreviousClosePrice: string;
   fluctuationsRatio: string;
-};
-
-const mapStockCodeToIcon = (symbolCode: string) => {
-  const mapping: { [key: string]: JSX.Element } = {
-    AAPL: <IconApple width={48} height={48} />,
-    AMZN: <IconAmazon width={48} height={48} />,
-    GOOGL: <IconGoogle width={48} height={48} />,
-    MSFT: <IconMs width={48} height={48} />,
-    NVDA: <IconNvidia width={48} height={48} />,
-    TSLA: <IconTesla width={48} height={48} />,
-    U: <IconUnity width={48} height={48} />,
-  };
-  const icon = mapping[symbolCode];
-  console.log(`Rendering icon for ${symbolCode}:`, icon);
-  return icon;
 };
 
 const changeClassName = (price: number) =>
@@ -54,19 +33,30 @@ export default function SrcStockList({
   return (
     <li className="flex justify-between items-center h-16">
       <div className="flex gap-4">
-        {mapStockCodeToIcon(symbolCode)}
+        {/* {mapStockCodeToIcon(symbolCode)} */}
+        <StockIcon symbolCode={symbolCode} width={48} height={48} />
         <div className="flex flex-col justify-center">
           <span className="text-sm font-bold grayscale-900">{stockName}</span>
-          <span className="text-sm font-regular grayscale-900">{stockCode}</span>
+          <span className="text-sm font-regular grayscale-900">
+            {stockCode}
+          </span>
         </div>
       </div>
       <div className="flex flex-col items-center">
-        <span className="text-sm grayscale-900 font-medium self-end">${closePrice}</span>
+        <span className="text-sm grayscale-900 font-medium self-end">
+          ${closePrice}
+        </span>
         <div className="flex gap-2">
-          <span className={`text-xs ${changeClassName(Number(compareToPreviousClosePrice))}`}>
+          <span
+            className={`text-xs ${changeClassName(
+              Number(compareToPreviousClosePrice),
+            )}`}
+          >
             {formatComparePrice(compareToPreviousClosePrice)}
           </span>
-          <span className={`text-xs ${changeClassName(Number(fluctuationsRatio))}`}>
+          <span
+            className={`text-xs ${changeClassName(Number(fluctuationsRatio))}`}
+          >
             {formatRatio(fluctuationsRatio)}%
           </span>
         </div>
